3ffd5a74267391ad0988fd45bd28b57323fa3855
Даниил Яндыбаев <xramirezx@outlook.com>
2020-10-13 04:35:24 +0400

    add callbacks

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -4,6 +4,12 @@ import MessengerMessage from './MessengerMessage/MessengerMessage';
 import { NavLink } from 'react-router-dom';
 
 const Messenger = (props) => {
+  const onSendClicked = () => {
+    alert(newMessageInput.current.value);
+  }
+
+  const newMessageInput = React.createRef();
+
   return (
     <section className="main__messenger messenger">
       <div className="messenger__dialogs dialogs">
@@ -17,14 +23,20 @@ const Messenger = (props) => {
         ) }
       </div>
       <div className="messenger__messages messages">
-        { props.state.messages.map( message => 
-          <MessengerMessage
-            from={ message.from }
-            avatar={ message.avatar }
-            fullName={ message.fullName }
-            text={ message.text }
-          />
-        ) }
+        <div className="messages__content">
+          { props.state.messages.map( message => 
+            <MessengerMessage
+              from={ message.from }
+              avatar={ message.avatar }
+              fullName={ message.fullName }
+              text={ message.text }
+            />
+          ) }
+        </div>
+        <div className="messages__new-message new-message">
+          <input ref={ newMessageInput }  className="new-message__input" type="text" name="message text" placeholder="Write a message..."/>
+          <button type="button" className="new-message__submit" onClick={ onSendClicked }>Send</button>
+        </div>
       </div>
     </section>
   )
--- src/components/Profile/ProfileNewPost/ProfileNewPost.jsx
+++ src/components/Profile/ProfileNewPost/ProfileNewPost.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const ProfileNewPost = () => {
+  let onSendClicked = () => {
+    let text = newPostInput.current.value;
+    alert(text);
+  };
+
+  let newPostInput = React.createRef();
+
   return (
     <div className="profile__block new-post">
-      <input class="new-post__input" type="text" name="post text" placeholder="What's in your mind?" />
-      <button type="button" class="new-post__submit">Send</button>
+      <input ref={ newPostInput } className="new-post__input" type="text" name="post text" placeholder="What's in your mind?" />
+      <button type="button" className="new-post__submit" onClick={ onSendClicked }>Send</button>
     </div>
   )
 }
