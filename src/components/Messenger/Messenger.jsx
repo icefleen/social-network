a3d85df79ab726d077029e4bbafb7c2748226753
Даниил Яндыбаев <xramirezx@outlook.com>
2020-11-06 23:34:49 +0400

    add reseting fields after submitting forms

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -10,6 +10,8 @@ import NewMessage from "./NewMessage/NewMessage";
 const Messenger = (props) => {
   const onSendClicked = (formData) => {
     props.addMessage(formData.messageText);
+    debugger;
+    props.reset("newMessageForm");
   };
 
   return (
