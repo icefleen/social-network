33e84bf27836436b2f657acadfbd81202807f4b3
Даниил Яндыбаев <xramirezx@outlook.com>
2020-11-09 06:52:18 +0400

    clean initialstate

--- src/store/reducers/profileReducer.js
+++ src/store/reducers/profileReducer.js
@@ -11,23 +11,7 @@ const initialState = {
   isLoading: false,
   status: "",
   friends: [],
-  posts: [
-    {
-      id: 1,
-      text:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim est eos nisi, totam esse reiciendis officiis, quidem similique debitis perspiciatis eaque sint harum atque amet maiores asperiores! Laudantium placeat doloremque eius sit quo earum cupiditate tempora minus. Quis repellat voluptas suscipit nisi, vitae aliquam saepe! Asperiores quasi error esse corrupti eligendi aspernatur facilis doloremque repudiandae unde libero atque, velit nulla cupiditate, facere itaque quam deserunt odit ut. Id est quas voluptas nisi, inventore voluptatem libero. Provident laboriosam, aspernatur eligendi amet, nesciunt placeat quod ipsam facere inventore incidunt praesentium vel cumque autem harum alias consequuntur porro facilis nihil? Porro, velit saepe.",
-      avatar: avatar,
-      fullName: "Daniil Yandybaev",
-      datetime: "56 minutes ago",
-    },
-    {
-      id: 2,
-      text: "Hello, World!",
-      avatar: avatar,
-      fullName: "Daniil Yandybaev",
-      datetime: "49 minutes ago",
-    },
-  ],
+  posts: [],
 };
 
 const profileReducer = (state = initialState, action) => {
